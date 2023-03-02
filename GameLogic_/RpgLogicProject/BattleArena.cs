433449a3ee8ec494c2b187ad66c2c0a6d591dff1
Malashin21d <113889092+Malashin21d@users.noreply.github.com>
2023-03-02 23:12:11 +0300

    Update BattleArena.cs

--- GameLogic_/RpgLogicProject/BattleArena.cs
+++ GameLogic_/RpgLogicProject/BattleArena.cs
@@ -1,33 +1,37 @@
-﻿using System;
+using System;
 namespace RpgLogicProject
 {
-    
-        public class BattleArena
+
+    public class BattleArena
+    {
+        public Enemy Enemy { get; set; }
+        public Hero Hero { get; set; }
+
+
+
+
+        public BattleArena(Enemy enemy, Hero hero)
         {
-            public Enemy Enemy { get; set; }
-            public Hero Hero { get; set; }
+            Enemy = enemy;
+            Hero = hero;
+
+        }
 
 
 
-
-            public BattleArena(Enemy enemy, Hero hero)
+        public int Battle()
+        {
+            Random rand = new Random();
+            int Chance;
+            var Enemy = new Enemy("Гоблин", 1, 70, 5);
+        start:
+            Console.WriteLine("1 - Напасть 2 - Прокрасться");
+            int ans = int.Parse(Console.ReadLine());
+            if (ans == 1)
             {
-                Enemy = enemy;
-                Hero = hero;
-                
-            }
-
-
-
-            public int Battle()
-            {
-                Random rand = new Random();
-                int Chance;
-                var Enemy = new Enemy("Гоблин", 1, 70, 5);
-            start:
-                Console.WriteLine("1 - Напасть 2 - Прокрасться");
-                int ans = int.Parse(Console.ReadLine());
-                if (ans == 1)
+                Console.WriteLine("Отравить клинок? 1 - Да 2 - нет");
+                int ansa = int.Parse(Console.ReadLine());
+                if (ansa == 1)
                 {
                     do
                     {
@@ -35,37 +39,90 @@ namespace RpgLogicProject
                         Console.WriteLine();
                         Console.WriteLine($"Героя здоровье: {Math.Round(Hero.Hp, 0)}");
                         Console.WriteLine($"Здоровье противника: {Math.Round(Enemy.Hp, 0)}");
-                        Chance = rand.Next(0, 2);
+                        Chance = rand.Next(0, 3);
                         if (Chance == 0)
                         {
                             Console.WriteLine($"Вы нанесли урона противнику: {Math.Round(Hero.Damage, 1)}");
-                            Enemy.Hp -= (Hero.Damage );
+                            Enemy.Hp -= (Hero.Damage);
+                            Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
+                            Hero.Hp -= Enemy.Damage;
                             if (Enemy.Hp <= 0)
                             {
                                 Console.WriteLine("Вы победили врага!");
                                 return 1;
+
                             }
                         }
                         else if (Chance == 1)
                         {
                             Console.WriteLine($"Вы нанесли крит противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
                             Enemy.Hp -= (Hero.Damage);
+                            Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
+                            Hero.Hp -= Enemy.Damage;
                             if (Enemy.Hp <= 0)
                             {
                                 Console.WriteLine("Вы победили врага!");
                                 return 1;
                             }
+                            else if (Chance == 2)
+                            {
+                                Console.WriteLine($"Вы застанили врага противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
+                                Enemy.Hp -= (Hero.Damage);
+                                if (Enemy.Hp <= 0)
+                                {
+                                    Console.WriteLine("Вы победили врага!");
+                                    return 1;
+
+                                }
+                            }
+
+                            if (Hero.Hp <= 0)
+                            {
+                                Console.WriteLine("Вы погибли!");
+                                return 0;
+                            }
+                            else { goto restart; }
                         }
-                        Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
-                        Hero.Hp -= Enemy.Damage;
-                        if (Hero.Hp <= 0)
-                        {
-                            Console.WriteLine("Вы погибли!");
-                            return 0;
+                        while (Enemy.Hp > 0 && Hero.Hp > 0) ;
                         }
-                        else { goto restart; }
+
+
+                    
+                    else if (ansa ==2)
+                    {
+                    do
+                    {
+                    restart:
+                            Console.WriteLine($"Вы нанесли крит противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
+                            Enemy.Hp -= (Hero.Damage);
+                            Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
+                            Hero.Hp -= Enemy.Damage;
+                            if (Enemy.Hp <= 0)
+                            {
+                                Console.WriteLine("Вы победили врага!");
+                                return 1;
+                            }
+                            else if (Chance == 2)
+                            {
+                                Console.WriteLine($"Вы застанили врага противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
+                                Enemy.Hp -= (Hero.Damage);
+                                if (Enemy.Hp <= 0)
+                                {
+                                    Console.WriteLine("Вы победили врага!");
+                                    return 1;
+
+                                }
+                            }
+
+                            if (Hero.Hp <= 0)
+                            {
+                                Console.WriteLine("Вы погибли!");
+                                return 0;
+                            }
+                            else { goto restart; }
+                        }
+                        while (Enemy.Hp > 0 && Hero.Hp > 0);
                     }
-                    while (Enemy.Hp > 0 && Hero.Hp > 0);
 
                 }
                 else if (ans == 2)
@@ -88,21 +145,30 @@ namespace RpgLogicProject
                             do
                             {
                             restart:
-                                Console.WriteLine();
-                                Console.WriteLine($"Ваше здоровье: {Math.Round(Hero.Hp, 0)}");
-                                Console.WriteLine($"Здоровье противника: {Math.Round(Enemy.Hp, 0)}");
-                                Console.WriteLine($"Вы нанесли урона противнику: {Math.Round(Hero.Damage, 1)}");
+                                Console.WriteLine($"Вы нанесли крит противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
                                 Enemy.Hp -= (Hero.Damage);
+                                Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
+                                Hero.Hp -= Enemy.Damage;
                                 if (Enemy.Hp <= 0)
                                 {
                                     Console.WriteLine("Вы победили врага!");
                                     return 1;
                                 }
-                                Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
-                                Hero.Hp -= Enemy.Damage;
+                                else if (Chance == 2)
+                                {
+                                    Console.WriteLine($"Вы застанили врага противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
+                                    Enemy.Hp -= (Hero.Damage);
+                                    if (Enemy.Hp <= 0)
+                                    {
+                                        Console.WriteLine("Вы победили врага!");
+                                        return 1;
+
+                                    }
+                                }
+
                                 if (Hero.Hp <= 0)
                                 {
-                                    Console.WriteLine("Вы погибли.");
+                                    Console.WriteLine("Вы погибли!");
                                     return 0;
                                 }
                                 else { goto restart; }
@@ -110,7 +176,7 @@ namespace RpgLogicProject
                             while (Enemy.Hp > 0 && Hero.Hp > 0);
 
                         }
-                        
+
                         else
                         {
                             goto start;
@@ -122,84 +188,12 @@ namespace RpgLogicProject
                         goto start;
                     }
                 }
-                else if (ans == 3)
-                {
-                    Chance = rand.Next(0, 2);
-                    if (Chance == 0)
-                    {
-                        Console.WriteLine("Вы подождали, пока противник не ушлёл по дальше и продолжали свой путь.");
-                        goto start;
-                    }
-                    else if (Chance == 1)
-                    {
-                        Console.WriteLine("Противник пошёл в вашу сторону и заметил вас");
-                        Console.WriteLine(" 1 - Вступить в бой");
-                        ans = int.Parse(Console.ReadLine());
-                        if (ans == 1)
-                        {
-                            do
-                            {
-                            restart:
-                                Console.WriteLine();
-                                Console.WriteLine($"Ваше здоровье: {Math.Round(Hero.Hp, 0)}");
-                                Console.WriteLine($"Здоровье противника: {Math.Round(Enemy.Hp, 0)}");
-                                Chance = rand.Next(0, 2);
-                                if (Chance == 0)
-                                {
-                                    Console.WriteLine($"Вы нанесли урона противнику: {Math.Round(Hero.Damage    , 1)}");
-                                    Enemy.Hp -= (Hero.Damage);
-                                    if (Enemy.Hp <= 0)
-                                    {
-                                        Console.WriteLine("Вы победили врага!");
-                                        return 1;
-                                    }
-                                }
-                                else if (Chance == 1)
-                                {
-                                    Console.WriteLine($"Вы нанесли крит противнику: {Math.Round(1.5 * (Hero.Damage), 1)}");
-                                    Enemy.Hp -= (Hero.Damage);
-                                    if (Enemy.Hp <= 0)
-                                    {
-                                        Console.WriteLine("Вы победили врага!");
-                                        return 1;
-                                    }
-                                }
-
-                                Console.WriteLine($"Вы получили урон: {Math.Round(Enemy.Damage, 1)}");
-                                Hero.Hp -= Enemy.Damage;
-                                if (Hero.Hp <= 0)
-                                {
-                                    Console.WriteLine("Вы погибли.");
-                                    return 0;
-                                }
-                                else { goto restart; }
-                            }
-                            while (Enemy.Hp > 0 && Hero.Hp > 0);
-                        }
-                        else if (Chance == 2)
-                        {
-                            goto start;
-                        }
-                        else
-                        {
-                            goto start;
-                        }
-
-                    }
-                    else
-                    {
-                        goto start;
-                    }
-
-                }
-                else
-                {
-                    goto start;
-                }
-
             }
-
         }
-
-
+    }
 }
+
+
+
+
+
